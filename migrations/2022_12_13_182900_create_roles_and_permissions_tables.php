9d8af320cf4759bc1d6fffe51aa6263ac71872bc
castaneras <dan@castaneras.com>
2022-12-13 20:15:00 -0500

    Update 2022_12_13_182900_create_roles_and_permissions_tables.php

--- migrations/2022_12_13_182900_create_roles_and_permissions_tables.php
+++ migrations/2022_12_13_182900_create_roles_and_permissions_tables.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-class CreateRolesAndPermissions extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
